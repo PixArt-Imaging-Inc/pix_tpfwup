1970df4adf5d0b94c71a909d7ee9629e59ee9e12
harris_tai <harris_tai@pixart.com>
2025-01-07 17:39:08 +0800

    feat: Version 0.1.5 -> 0.1.6
    1. Re-upload the asset files for the GitHub release page.

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ using namespace std;
 using namespace pixart;
 
 #define TITLE       "Pixart Touchpad Utility"
-#define VERSION_STR "v0.1.5"
+#define VERSION_STR "v0.1.6"
 
 int main(int argc, char **argv)
 {
